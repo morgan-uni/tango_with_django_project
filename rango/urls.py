05473790ee409c5a3b09810ee5c6f23c6e460e7a
morgan-uni <2443093d@student.gla.ac.uk>
2021-02-10 01:07:07 +0000

    chapter 6 tests completed

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from rango import views
 app_name = 'rango'
 urlpatterns = [
-path('', views.index, name='index'),
-path('about/', views.about, name='about'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+        views.show_category, name='show_category'),
+    #path('page/<slug:page_name_slug>/',
+        #views.show_category, name='show_page'),
 ]
